b80f898f86e21e1af0f14f39406afa5999e7f828
JeongSeonHighSchool <103098800+JeongSeonHighSchool@users.noreply.github.com>
2022-10-18 01:00:04 +0900

    Add files via upload

--- LCD_1.ino
+++ LCD_1.ino
@@ -1,3 +1,5 @@
+#include <Servo.h>
+
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #include "DHT.h"
@@ -9,14 +11,21 @@
 DHT dht(DHTPIN, DHTTYPE);
  
 LiquidCrystal_I2C lcd(0x27, 16, 2);
- 
- 
+Servo servo;
+int value = 0;
+int pan = 22;
+int pan_2 = 30;
+int pan_3 = 36;
 void setup()
 {
   lcd.init();
   lcd.backlight();
   pinMode(3,OUTPUT);
+  pinMode(pan,OUTPUT);
+  pinMode(pan_2,OUTPUT);
+  pinMode(pan_3,OUTPUT);
   
+  servo.attach(7);
   Serial.begin(9600);
 }
  
@@ -43,16 +52,40 @@ void loop()
 
   lcd.print("'C");
   delay(1000);
-  if(h>=80){
-    //servo 작동 가습기 off    
+  
+  if(h>=90){
+    value = 90;  
+    servo.write(value);
+    delay(500);
+    value = 0;
+    servo.write(value);
+    delay(500);
+    value = 90;
+    servo.write(value);
+    delay(500);
     digitalWrite(3,LOW);
+    digitalWrite(pan,HIGH);
+    digitalWrite(pan_2,HIGH);
+    digitalWrite(pan_3,HIGH);
     delay(1000);
   }
-   else if(h<=60){
-    //servo 작동 가습기 on
+   else if(h<=70){
+    value = 90;  
+    servo.write(value);
+    delay(500);
+    value = 0;
+    servo.write(value);
+    delay(500);
+    value = 90;
+    servo.write(value);
+    delay(500);//servo 작동 가습기 on
     digitalWrite(3,HIGH);
+    digitalWrite(pan,LOW);
+    digitalWrite(pan_2,LOW);
+    digitalWrite(pan_3,LOW);
     delay(1000);
-   }
     
+   }
+   
   
 }
